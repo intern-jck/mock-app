7eb7c7424751b5fd0c2667b890bfe711ccd4ef94
intern-laptop <justin.c.kirk@gmail.com>
2022-10-19 20:30:17 -0400

    creates product info mvp

--- client/components/Product/StyleSelector/StyleSelector.jsx
+++ client/components/Product/StyleSelector/StyleSelector.jsx
@@ -1,85 +1,24 @@
 import React from 'react'
 import {FaCheck} from 'react-icons/fa';
+import './StyleSelector.css';
 
-function StyleSelector({styleHandler, style, results}) {
-
-  const chunk = (array, size) => {
-    let selectedStyle = {};
-    let unselectedStyles = [];
-
-    for (let i in array) {
-      if (array[i].style_id !== style.style_id) {
-        unselectedStyles.push(array[i])
-      }
-    }
-
-    unselectedStyles.unshift(style);
-
-    return unselectedStyles.reduce((chunks, item, i) => {
-      if (i % size === 0) {
-        chunks.push([item]);
-      } else {
-        chunks[chunks.length - 1].push(item);
-      }
-      return chunks;
-    }, []);
-
-  }
-
-  const styleRows = chunk( results? results : [], 4);
-
-  const clickHandler = (event) => {
-    const target = event.target;
-    const name = target.name;
-
-    for ( let result of results) {
-      if (parseInt(name) === result.style_id) {
-        styleHandler(result);
-      }
-    }
-  };
+function StyleSelector({styles}) {
+  console.log(styles.styles.photos)
 
   return (
-    <div className="overview-style-selector">
-
-      <div className="selector-header">
-        <h5>STYLE > {style.name} </h5>
-      </div>
-
-      <div className="selector-body">
-        {
-          styleRows.length > 1 ? styleRows.map((row, i) => {
-
-            return (
-              <div key={i} className="selector-row">{
-                row.map((col) => {
-
-                  let checkDiv = <></>
-
-                  if ( col.style_id === style.style_id) {
-                    checkDiv = <FaCheck className="thumb-checkmark" size={25}/>
-                  }
-
-                  return (
-                    <div key={col.style_id} className="thumb-div">
-                      <img
-                        className="selector-thumb onclick"
-                        name={col.style_id}
-                        onClick={clickHandler}
-                        src={col.photos[0].thumbnail_url}>
-                      </img>
-                      { checkDiv }
-                    </div>
-                  )
-                })
-              }</div>
-            )
-          }) : null
-        }
-      </div>
-
+    <div className="StyleSelector">
+      {
+        styles.styles.map((style, i) => {
+          return (
+            <div key={i} className='style-div'>
+              test
+              {/* <img src={style.photos[0]}/> */}
+            </div>
+          )
+        })
+      }
     </div>
-  )
+  );
 }
 
 export default StyleSelector
