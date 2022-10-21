63dcec129e0adb8808bf86bc558332a81c9113ff
intern-laptop <justin.c.kirk@gmail.com>
2022-10-21 12:38:07 -0400

    server mvp

--- server/controllers/questionsController.js
+++ server/controllers/questionsController.js
@@ -3,7 +3,6 @@ const Answer = require('../models/answerModel.js');
 
 // GET REQ
 const getQuestions = (req, res) => {
-  console.log('Get Questions',typeof parseInt(req.params.product_id))
   Question.find({'product_id': parseInt(req.params.product_id)})
   // .limit(1)
   // .sort({'product_id': 1})
@@ -13,7 +12,6 @@ const getQuestions = (req, res) => {
   // })
   .exec()
   .then((doc) => {
-    console.log('got', doc)
     res.send(doc);
   })
   .catch((error) => {
@@ -23,7 +21,6 @@ const getQuestions = (req, res) => {
 };
 
 const getAnswers = (req, res) => {
-  console.log('Get Answers', req.params.question_id);
   Question.findOne(
     {'results.question_id': req.params.question_id},
     {'results': 1, '_id': 0}
@@ -34,7 +31,7 @@ const getAnswers = (req, res) => {
     doc.results.forEach((result) => {
       // console.log(result.question_id, req.params.question_id)
       if (result.question_id === parseInt(req.params.question_id)) {
-        console.log(result)
+        // console.log(result)
         res.send(result);
       }
     })
